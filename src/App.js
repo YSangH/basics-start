91b1b4f1bf39e6060f549854844c7b9f5ccae2d4
DevYsh <ysh940129@gmail.com>
2024-09-30 14:08:39 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpensiveItem from "./components/ExpensiveItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpensiveItem />
+    </div>
+  );
+}
+
+export default App;
