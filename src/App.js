3db40c7590eeed01b5996c059314c7295c2c8040
유상현 <ysh940129@gmail.com>
2024-09-30 05:27:32 +0000

    2024.9.30

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import ExpensiveItem from "./components/ExpensiveItem";
 
 function App() {
+  const expenses = [
+    // props = title, price, date
+    { id: "cost1", title: "자동차보험", price: "180000", date: "2024.9.30" },
+    { id: "cost2", title: "주유비", price: "30000", date: "2024.9.29" },
+    { id: "cost3", title: "정비", price: "100000", date: "2024.9.28" },
+    { id: "cost4", title: "기타 부품", price: "50000", date: "2024.9.27" },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpensiveItem />
+      <ExpensiveItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpensiveItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpensiveItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
+      <ExpensiveItem
+        title={expenses[3].title}
+        price={expenses[3].price}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
--- src/components/ExpensiveItem.jsx
+++ src/components/ExpensiveItem.jsx
@@ -1,24 +1,14 @@
 import "./ExpensiveItem.css";
 
-export default function ExpensiveItem() {
+export default function ExpensiveItem(props) {
   // 다른 값으로 변경하기
-  const today = new Date();
-
-  const year = today.getFullYear(); // 연도 (예: 2024)
-  const month = String(today.getMonth() + 1).padStart(2, "0"); // 월 (0부터 시작하므로 1을 더함)
-  const day = String(today.getDate()).padStart(2, "0"); // 일
-
-  const expenseDate = `${year}-${month}-${day}`;
-
-  const expenseTitle = "자동차보험";
-  const expensePrice = "180000";
 
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{props.date}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expensePrice}원</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.price}원</div>
       </div>
     </div>
   );
