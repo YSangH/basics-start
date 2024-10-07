c6556d20c90df150e9bde8f77aa4220dc5ee41dd
유상현 <ysh940129@gmail.com>
2024-10-07 05:55:44 +0000

    finish

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,19 @@
 import ExpensiveItem from "./ExpensiveItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function items(props) {
+  const [filterYear, setFilterYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filterYear} onChange={filterChangeHandler} />
       <ExpensiveItem
         title={props.items[0].title}
         price={props.items[0].price}
