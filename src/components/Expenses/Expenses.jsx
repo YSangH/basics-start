2fe33c31dcbdbaf1b3a4331b446a8ddfaa6b5939
유상현 <ysh940129@gmail.com>
2024-10-10 03:58:02 +0000

    2024.10.10

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -14,26 +14,13 @@ export default function items(props) {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onChange={filterChangeHandler} />
-      <ExpensiveItem
-        title={props.items[0].title}
-        price={props.items[0].price}
-        date={props.items[0].date}
-      />
-      <ExpensiveItem
-        title={props.items[1].title}
-        price={props.items[1].price}
-        date={props.items[1].date}
-      />
-      <ExpensiveItem
-        title={props.items[2].title}
-        price={props.items[2].price}
-        date={props.items[2].date}
-      />
-      <ExpensiveItem
-        title={props.items[3].title}
-        price={props.items[3].price}
-        date={props.items[3].date}
-      />
+      {props.items.map((expense) => (
+        <ExpensiveItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
